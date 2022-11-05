2c3d8a3e467fa10c2a33716918edd0af4c71a152
Joseph Adewunmi <adedejiosvaldo@gmail.com>
2022-11-05 16:05:11 +0100

    fix:added validation for message and email

--- src/component/ContactForm.jsx
+++ src/component/ContactForm.jsx
@@ -1,100 +1,179 @@
-import React,{useState} from "react";
-import  contactForm from "../styles/contactForm.module.css";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import contactForm from "../styles/contactForm.module.css";
 export const ContactForm = () => {
-    const [first, setfirst] = useState(false)
-    const [second, setSecond] = useState(false)
-    const [firstname, setFirstname] = useState('')
-    const [lastName, setLastName] = useState('')
+  const navigate = useNavigate();
+  const [first, setfirst] = useState(false);
+  const [message, setMessage] = useState(false);
+  const [second, setSecond] = useState(false);
+  const [messageError, setMessageError] = useState(false);
+  const [buttonDisabled, setButtonDisabled] = useState(true);
+  const [firstname, setFirstname] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [checkbox, setCheckbox] = useState(false);
 
+  const firstNameHandler = (e) => {
+    setFirstname(e.target.value);
 
-const firstNameHandler = (e)=>{
-    setFirstname(e.target.value)
+    if (firstname.length <= 0) {
+      setfirst(true);
+    } else {
+      setfirst(false);
+    }
+  };
 
-    if (firstname.length <= 0)  {
-        setfirst(true)
+  const secondNameHandler = (e) => {
+    setLastName(e.target.value);
 
-    } else{
-        setfirst(false)
+    if (lastName.length <= 0) {
+      setSecond(true);
+    } else {
+      setSecond(false);
+    }
+  };
+  const messageHandler = (e) => {
+    setMessage(e.target.value);
 
+    if (message.length <= 0) {
+      setMessageError(true);
+    } else {
+      setMessageError(false);
+    }
+  };
+  const formHandler = (e) => {
+    e.preventDefault();
+    if (firstname.length <= 0) {
+      setfirst(true);
+    } else {
+      setfirst(false);
     }
 
-}
-
-
-const secondNameHandler = (e)=>{
-    setLastName(e.target.value)
-
-    if (lastName.length <= 0)  {
-        setSecond(true)
-
-    } else{
-        setSecond(false)
-
+    if (lastName.length <= 0) {
+      setSecond(true);
+    } else {
+      setSecond(false);
     }
+  };
 
-}
-    const formHandler = (e) => {
-    e.preventDefault()
-    if (firstname.length <= 0)  {
-        setfirst(true)
-
-    } else{
-        setfirst(false)
-
-    }
-
-
-    if (lastName.length <= 0)  {
-        setSecond(true)
-
-    } else{
-        setSecond(false)
-
-    }
-}
-
+  const checkboxHandler = () => {
+    setCheckbox(true);
+    setButtonDisabled(false);
+  };
   return (
     <div className={contactForm.container}>
-      <form onSubmit={formHandler}>
-      <h2>Contact Me</h2>
+      <form action='POST' onSubmit={formHandler}>
+        <h2>Contact Me</h2>
 
-       <div className={contactForm.name__div}>
-
-       <div id={contactForm.firstName}>
-        <label htmlFor={contactForm.first_name}>First Name
-            <input id={contactForm.first_name} type='text' value={firstname} onChange={firstNameHandler}
-            style={{borderColor:  first?'red' :''}} placeholder="Enter your first name" />
-            <p style={{fontSize:'10px',color:'red',marginTop:'-15px',}}>{first ? <h3>You need to fill in a name</h3>: ''}</p>
+        <div className={contactForm.name__div}>
+          <div id={contactForm.firstName}>
+            <label htmlFor={contactForm.first_name}>
+              First Name
+              <input
+                id={contactForm.first_name}
+                type='text'
+                value={firstname}
+                onChange={firstNameHandler}
+                style={{ borderColor: first ? "red" : "" }}
+                placeholder='Enter your first name'
+              />
+              <p style={{ fontSize: "10px", color: "red", marginTop: "-15px" }}>
+                {first ? <h3>You need to fill in a name</h3> : ""}
+              </p>
             </label>
-            </div>
+          </div>
 
+          <div>
+            <label htmlFor={contactForm.last_name}>
+              {" "}
+              Last Name
+              <input
+                id={contactForm.last_name}
+                type='text'
+                value={lastName}
+                style={{ borderColor: second ? "red" : "" }}
+                onChange={secondNameHandler}
+                placeholder='Enter your last name'
+              />
+              <p style={{ fontSize: "10px", color: "red", marginTop: "-15px" }}>
+                {second ? <h3>You need to fill in a name</h3> : ""}
+              </p>
+            </label>
+          </div>
+        </div>
 
-            <div>
-            <label  htmlFor={contactForm.last_name}> Last Name
-            <input id={contactForm.last_name} type='text' value={lastName} style={{borderColor:  second?'red' :''}} onChange={secondNameHandler} placeholder="Enter your last name" />
-            <p style={{fontSize:'10px',color:'red',marginTop:'-15px',}}>{second ? <h3>You need to fill in a name</h3>: ''}</p>
+        <label htmlFor={contactForm.email}>
+          Email
+          <input
+            id={contactForm.email}
+            type='email'
+            placeholder='yourname@email.com'
+          />
+          <p
+            style={{
+              fontSize: "10px",
+              fontWeight: "400",
+              color: "red",
+              marginTop: "-15px",
+            }}
+          >
+            {second ? <h3>Email Address is required</h3> : ""}
+          </p>
         </label>
 
-       </div>
-
-       </div>
-        <label htmlFor={contactForm.email}>Email
-        <input id={contactForm.email} type='email' placeholder="yourname@email.com" />
+        <label htmlFor={contactForm.message}>
+          Message
+          <textarea
+            id={contactForm.message}
+            rows='3'
+            cols='40'
+            value={message}
+            onChange={setMessage}
+            type='text'
+            placeholder="Send me a message and i'll reply you as soon as possible"
+          />
+          <p
+            style={{
+              fontSize: "10px",
+              fontWeight: "400",
+              color: "red",
+              marginTop: "-15px",
+            }}
+          >
+            {second ? <h3>Message Cannot be empty</h3> : ""}
+          </p>
         </label>
 
-        <label htmlFor={contactForm.message }>Message</label>
-        <textarea id={contactForm.message} rows='3' cols='40' type='text'
-        placeholder="Send me a message and i'll reply you as soon as possible"/>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "left",
+            justifyItems: "center",
+            alignItems: "center",
+            padding: "10px",
+          }}
+        >
+          <input
+            type='checkbox'
+            checked={checkbox}
+            onClick={checkboxHandler}
+            id={contactForm.checkbox__check}
+          />
+          <label id={contactForm.label} htmlFor={contactForm.checkbox__check}>
+            You agree to providing your data to Joseph who may contact you
+          </label>
+        </div>
 
-
-       <div style={{display:'flex',alignItems:'center',}}>
-       <input type='checkbox' id={contactForm.checkbox__check} />
-        <label  htmlFor={contactForm.checkbox__check}>
-     You agree to providing your data to Joseph who may contact you</label>
-       </div>
-
-
-        <button id={contactForm.button} style={{backgroundColor:'blue'}}>Send Message</button>
+        <button
+          type='submit'
+          id={contactForm.button}
+          disabled={buttonDisabled}
+          style={{
+            backgroundColor: "#175CD3",
+            opacity: buttonDisabled ? "40%" : "100",
+          }}
+        >
+          Send Message
+        </button>
       </form>
     </div>
   );
